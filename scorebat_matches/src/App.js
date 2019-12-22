7b969b77afbb46d25bd2db3d260625f46fda8589
George Trapp <gtrapp@gmail.com>
2019-12-22 11:55:33 -0500

    starting with table

---
+++ scorebat_matches/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import DatatablePage from './Table.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DatatablePage/>
+    </div>
+  );
+}
+
+export default App;
