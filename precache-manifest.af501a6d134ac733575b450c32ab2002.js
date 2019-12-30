5926f280556950d543de2e08a845e07153eba3cc
George Trapp <gtrapp@gmail.com>
2019-12-30 17:38:24 -0500

    Updates

---
+++ precache-manifest.af501a6d134ac733575b450c32ab2002.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce7b3b0a929e344ffc060c6fe86f050",
+    "url": "/nvideohighlights/index.html"
+  },
+  {
+    "revision": "e6b444681216b3c65dec",
+    "url": "/nvideohighlights/static/css/2.4058c193.chunk.css"
+  },
+  {
+    "revision": "eb7003745fd2f5f9237d",
+    "url": "/nvideohighlights/static/css/main.0d6dc19f.chunk.css"
+  },
+  {
+    "revision": "e6b444681216b3c65dec",
+    "url": "/nvideohighlights/static/js/2.25bebb73.chunk.js"
+  },
+  {
+    "revision": "30d8ef3813de0269a8460a5317dd0eba",
+    "url": "/nvideohighlights/static/js/2.25bebb73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb7003745fd2f5f9237d",
+    "url": "/nvideohighlights/static/js/main.097da266.chunk.js"
+  },
+  {
+    "revision": "5afbe299b95aeb1885a8",
+    "url": "/nvideohighlights/static/js/runtime-main.559870b2.js"
+  }
+]);
